85ab3bb07fe1366893ef3bfa13601bb1ef91b78f
Samuela Pollack <sampoll@users.noreply.github.com>
2017-12-11 14:59:21 -0500

    Automatic cluster for batchtools with SSH

---
+++ SSH-CLUSTER/delete.py
@@ -0,0 +1,15 @@
+import boto3
+import pickle
+
+file = open('cluster.obj', 'rb')
+cluster = pickle.load(file)
+file.close()
+
+client = boto3.client('ec2')
+res = client.terminate_instances(InstanceIds=cluster['iids'])
+
+for id in cluster['iids']:
+  boto3.resource('ec2').Instance(id).wait_until_terminated()
+client.delete_security_group(GroupName=cluster['sgname'])
+
+
